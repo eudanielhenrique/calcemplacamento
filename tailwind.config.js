2b5d49d4b87e3e65e1f60996f61fd5e6e6b5fc55
Daniel Henrique <ddanielhpf@gmail.com>
2025-04-03 10:49:13 -0300

    build(tailwind): update content paths to include components directory
    Modify the content paths in the Tailwind configuration to ensure styles are applied to components in the `src/components` directory. This change ensures that Tailwind processes all relevant files correctly.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./dist/**/*.{html,js}"
+    "./src/components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
